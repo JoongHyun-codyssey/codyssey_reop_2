1dfc9fbea222a73b8030584b722479aa45ed2e80
JoongHyun-codyssey <https://github.com/JoongHyun-codyssey>
2026-07-31 13:16:19 +0900

    feat: Quiz, QuizGame 클래스 초기 설정

---
+++ quiz.py
@@ -0,0 +1,4 @@
+class Quiz:
+    def __init__(self, questions, answers):
+        self.questions = questions
+        self.answers = answers
---
+++ quiz_game.py
@@ -0,0 +1,19 @@
+class QuizGame:
+    def __init__(self):
+        self.quiz_list = []
+        self.score = 0
+
+    def guess_quiz(self):
+        return "퀴즈 맞추기"
+
+    def add_quiz(self):
+        return "퀴즈 추가하기"
+
+    def list_quiz(self):
+        return "퀴즈 목록 보기"
+
+    def check_score(self):
+        return "점수 확인하기"
+
+    def end_quiz(self):
+        return "퀴즈 끝내기"
