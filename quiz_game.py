b3083b99e7d69e70fde6b90f10a1d7e2eee8157c
JoongHyun-codyssey <https://github.com/JoongHyun-codyssey>
2026-08-01 19:44:39 +0900

    feat:파일 저장 및 불러오기 구현 및 json 초기 데이터 로딩 로직 개선

--- quiz_game.py
+++ quiz_game.py
@@ -1,7 +1,9 @@
+import json
+
 class QuizGame:
     def __init__(self):
-        self.quiz_list = []
-        self.score = 0
+        self.quiz_list = self.load_quiz_list()
+        self.best_score = self.load_best_score()
 
     def guess_quiz(self):
         return "퀴즈 맞추기"
@@ -17,3 +19,26 @@ class QuizGame:
 
     def end_quiz(self):
         return "퀴즈 끝내기"
+
+    def load_quiz_list(self):
+        try:
+            with open("state.json", mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+                return data.get("quiz_list") or data.get("modern_quiz_list", [])
+        except (FileNotFoundError, json.JSONDecodeError):
+            return 0
+
+    def load_best_score(self):
+        try:
+            with open("state.json", mode="r", encoding="utf-8") as file:
+                data = json.load(file)
+                return data.get("best_score", 0)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return 0
+
+    def save_best_score(self):
+            data = {
+                "best_score" : self.best_score
+            }
+            with open("state.json", mode="w", encoding="utf-8") as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
