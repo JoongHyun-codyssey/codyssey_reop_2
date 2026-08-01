41536dd2c950e287b65476624ca07c786d2fe016
JoongHyun-codyssey <https://github.com/JoongHyun-codyssey>
2026-08-01 20:36:40 +0900

    feat: 딕셔너리 변환 함수 추가

--- quiz.py
+++ quiz.py
@@ -1,6 +1,14 @@
 class Quiz:
     def __init__(self, id, question, choices, answer):
-        self.quiz_id = id
-        self.questions = question
+        self.id = id
+        self.question = question
         self.choices = choices
-        self.answers = answer
+        self.answer = answer
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "question": self.question,
+            "choices": self.choices,
+            "answer": self.answer
+        }
